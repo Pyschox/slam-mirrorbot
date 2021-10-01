c6cb20e8464be091a5042672f92f7b889f87e082
Pyschox <88329428+Pyschox@users.noreply.github.com>
2021-10-01 22:11:37 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -807,9 +807,9 @@ class GoogleDriveHelper:
 
         for content in self.telegraph_content :
             self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                    title = 'Slam Mirrorbot Search',
-                                                    author_name='Slam Mirrorbot',
-                                                    author_url='https://github.com/SlamDevs/slam-mirrorbot',
+                                                    title = 'Ad Mirror Bot',
+                                                    author_name='Kal Ho Na Ho,
+                                                    author_url='https://t.me/psychoxdevil',
                                                     html_content=content
                                                     )['path'])
 
