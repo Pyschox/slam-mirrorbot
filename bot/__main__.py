8b4adb8af9681819659f054997991cdb33f17234
Pyschox <88329428+Pyschox@users.noreply.github.com>
2021-10-01 13:27:52 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("Group", "https://t.me/joinchat/HjOmDe_9kbw4YTll")
+    buttons.buildbutton("Channel", "https://t.me/joinchat/bahP8CXhGSYxMGM1")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
