8d67d604df2bd078c37fc7e02509cf37c36e75ac
Pyschox <88329428+Pyschox@users.noreply.github.com>
2021-10-02 13:38:44 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Group", "https://t.me/joinchat/HjOmDe_9kbw4YTll")
-    buttons.buildbutton("Channel", "https://t.me/joinchat/bahP8CXhGSYxMGM1")
+    buttons.buildbutton("💬 Group 💬", "https://t.me/joinchat/HjOmDe_9kbw4YTll")
+    buttons.buildbutton("👨‍💻 Dev 👨‍💻", "http://t.me/psychoxdevil")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
